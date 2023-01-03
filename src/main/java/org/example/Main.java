fcd4b3411147089f2985e238d936f4988dbc6cfd
Ray <rfadkins@hotmail.com>
2023-01-03 16:41:07 -0500

    Code around file input framework complete. Next Step: Actual file input.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,32 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    // a global scanner for your global scanning needs. (Watch me only need it once, now)
+    static Scanner fileScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Check back later.");
-        System.out.println("We're kind of basic at the moment.");
-        String fileName = "test.txt";
+
+
+        //Get a filename and pull the contents of that file
+        String fileName = fileEntry();
         List<String> itemList = new ArrayList<>();
         itemList.addAll(TXT2ArrayList.convert(fileName));
 
-
         //Test Outputs
         listMyList(itemList);
+
+        System.out.println("Check back later.");
+        System.out.println("We're kind of basic at the moment ;)");
+    }
+
+    public static String fileEntry() {
+        System.out.println("Please Enter the name of the file:");
+        String fileName = fileScanner.nextLine();
+        return fileName;
     }
 
     public static void listMyList(List<String> myList) {
